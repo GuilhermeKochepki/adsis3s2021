3277b090e597eda8a12d9c7520d78e830f2409fe
GuilhermeKochepki <gui.kochepki@gmail.com>
2021-06-21 10:11:57 -0300

    Atualização atividade de repositório com astah

--- src/main/java/meu/aula20210608/AppPessoa.java
+++ src/main/java/meu/aula20210608/AppPessoa.java
@@ -1,6 +1,10 @@
 package meu.aula20210608;
 
 import meu.aula20210608.repositorio.PessoaRepository;
+
+import javax.swing.JOptionPane;
+
+import meu.aula20210608.pessoa.Pessoa;
 import meu.aula20210608.pessoa.PessoaFisica;
 import meu.aula20210608.pessoa.PessoaJuridica;
 import meu.aula20210608.valueObjects.CPF;
@@ -12,6 +16,10 @@ public class AppPessoa {
 		try {
 			PessoaRepository repo = new PessoaRepository();
 			
+			repo.excluirPeloId(55);
+			repo.excluirPeloId(77);
+			repo.excluirPeloId(21);
+			
 			PessoaFisica ana = new PessoaFisica(55, "Ana Maria", new CPF("545.788.865-14"));
 			PessoaFisica joaquim = new PessoaFisica(77, "Joaquim Silva", new CPF("123.544.865-55"));
 			PessoaJuridica ibm = new PessoaJuridica(21, "IBM INC", new CNPJ("54.554.554/0001-33"));
@@ -19,6 +27,18 @@ public class AppPessoa {
 			repo.incluir(ana);
 			repo.incluir(joaquim);
 			repo.incluir(ibm);
+			
+			JOptionPane.showConfirmDialog(null, "Opa, incluídos!");
+			
+			Pessoa recuperadaPeloId = repo.recuperarPeloid(77);
+			System.out.println("Pessoa recuperada pelo id: " 
+					+ recuperadaPeloId.getNome() + "  "
+					+ recuperadaPeloId.getId() + "  "
+					+ recuperadaPeloId.getClass());
+			JOptionPane.showConfirmDialog(null, "Vamos excluir o 21!");
+			
+			repo.excluirPeloId(21);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/main/java/meu/aula20210608/repositorio/PessoaRepository.java
+++ src/main/java/meu/aula20210608/repositorio/PessoaRepository.java
@@ -3,10 +3,13 @@ package meu.aula20210608.repositorio;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import meu.aula20210608.pessoa.Pessoa;
 import meu.aula20210608.pessoa.PessoaFisica;
 import meu.aula20210608.pessoa.PessoaJuridica;
+import meu.aula20210608.valueObjects.CNPJ;
+import meu.aula20210608.valueObjects.CPF;
 
 public class PessoaRepository {
 	private Connection conexão;
@@ -43,6 +46,68 @@ public class PessoaRepository {
 		conexão.createStatement().execute(sqlCreateFisica);
 		conexão.createStatement().execute(sqlCreateJuridica);
 		conexão.commit();
+	}
+	
+	public Pessoa recuperarPeloid(long id) throws Exception {
+		Pessoa recuperada = null;
+		try {
+			PreparedStatement psSelectPessoa = conexão.prepareStatement("select id, nome, tipo_pessoa from pessoa where id = ?");
+			PreparedStatement psSelectFisica = conexão.prepareStatement("select id, cpf from pessoaFisica where id = ?");
+			PreparedStatement psSelectJuridica = conexão.prepareStatement("select id, cnpj from pessoaJuridica where id = ?");
+			
+			psSelectPessoa.setLong(1,id);
+			ResultSet rsPessoa = psSelectPessoa.executeQuery();
+			if (rsPessoa.next()) {
+				String nome = rsPessoa.getString("nome");
+				String tipoPessoa = rsPessoa.getString("tipo_pessoa");
+				if (tipoPessoa.equals("FISICA")) {
+					psSelectFisica.setLong(1,id);
+					ResultSet rsFisica = psSelectFisica.executeQuery();
+					if (rsFisica.next()) {
+						recuperada = new PessoaFisica(id, nome, new CPF(rsFisica.getString("cpf")));	
+						return recuperada;
+					}
+				} else if (tipoPessoa.equals("JURIDICA")) {
+					psSelectJuridica.setLong(1,id);
+					ResultSet rsJuridica = psSelectJuridica.executeQuery();
+					if (rsJuridica.next()) {
+						recuperada = new PessoaJuridica(id, nome, new CNPJ(rsJuridica.getString("cnpj")));	
+						return recuperada;
+					}
+				}
+			} else {
+				throw new RuntimeException("Pessoa não encontrada!");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return recuperada;
+	}
+	
+	public void excluirPeloId(long id) throws Exception {
+		try {
+			PreparedStatement psDeletePessoa = conexão.prepareStatement("delete from pessoa where id = ?");
+			PreparedStatement psDeleteFisica = conexão.prepareStatement("delete from pessoaFisica where id = ?");
+			PreparedStatement psDeleteJuridica = conexão.prepareStatement("delete from pessoaJuridica where id = ?");
+			
+			psDeletePessoa.setLong(1, id);
+			psDeleteFisica.setLong(1, id);
+			psDeleteJuridica.setLong(1, id);
+			
+			psDeleteJuridica.execute();
+			psDeleteFisica.execute();
+			psDeletePessoa.execute();
+			
+			psDeleteFisica.close();
+			psDeleteJuridica.close();
+			psDeleteFisica.close();
+			conexão.commit();
+		} catch (Exception e) {
+			conexão.rollback();
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public void incluir (Pessoa pessoa) throws Exception {
